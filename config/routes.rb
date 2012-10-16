fa55a2712558e2c5275bcf969ec92a33ed18bba4
Kentaro Kuribayashi <kentarok@gmail.com>
2012-10-16 19:48:49 +0900

    Routes / to service index page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Hyperion::Application.routes.draw do
-  root to: 'Root#index'
-  get  '/caveat', to: 'Root#caveat'
+  root to: 'services#index'
+  get  '/caveat', to: 'root#caveat'
 
   get    '/signin' => redirect('/auth/github')
   delete '/signout', to: 'sessions#destroy'
