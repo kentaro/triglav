ba12fd8644220dee801344af1dcc5feaa3466be9
Kentaro Kuribayashi <kentarok@gmail.com>
2012-10-17 18:46:47 +0900

    Fixed inappropriate deletion.

--- app/models/role.rb
+++ app/models/role.rb
@@ -6,7 +6,7 @@ class Role < ActiveRecord::Base
   validates :name,        uniqueness: true, presence: true, length: { maximum:  100 }
   validates :description, length: { maximum: 1000 }
 
-  has_many :host_relations
+  has_many :host_relations, dependent: :delete_all
   has_many :hosts, -> { where deleted_at: nil },
             through:    :host_relations,
             class_name: 'Host',
--- app/models/service.rb
+++ app/models/service.rb
@@ -6,7 +6,7 @@ class Service < ActiveRecord::Base
   validates :name,        uniqueness: true, presence: true, length: { maximum:  100 }
   validates :description, length: { maximum: 1000 }
 
-  has_many :host_relations
+  has_many :host_relations, dependent: :delete_all
   has_many :roles, through: :host_relations
   has_many :activities, as: :model
 
