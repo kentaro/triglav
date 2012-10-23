92bf1b0016709f5a4d9348d5c8730c6783560b57
Kentaro Kuribayashi <kentarok@gmail.com>
2012-10-23 15:40:42 +0900

    Added setting item `munin` to Service #6

--- app/models/service.rb
+++ app/models/service.rb
@@ -5,6 +5,7 @@ class Service < ActiveRecord::Base
 
   validates :name,        uniqueness: true, presence: true, length: { maximum:  100 }
   validates :description, length: { maximum: 1000 }
+  validates :munin_url,   format: URI::regexp(%w(http https)), allow_blank: true
 
   has_many :host_relations, dependent: :delete_all
   has_many :roles, through: :host_relations
@@ -22,5 +23,27 @@ class Service < ActiveRecord::Base
 
   def roles_with_hosts
     roles.includes(:hosts).references(:hosts).uniq
+  end
+
+  def munin_url_for_service
+    uri      = URI.parse(self.munin_url)
+    path     = uri.path ? uri.path.sub(/\/$/, '') : ''
+    uri.path = [path, self.name].map { |s| URI.escape(s) }.join('/')
+    uri
+  end
+
+  def munin_url_for_service_and (host)
+    uri      = URI.parse(munin_url)
+    path     = uri.path ? uri.path.sub(/\/$/, '') : ''
+    uri.path = [path, name, host.name].map { |s| URI.escape(s) }.join('/')
+    uri
+  end
+
+  def munin_url_for_graph_of (host, options = {})
+    options  = { type: :load, span: :day}.merge(options)
+    uri      = munin_url_for_service_and(host)
+    path     = uri.path ? uri.path.sub(/\/$/, '') : ''
+    uri.path = [path, "#{options[:type].to_s}-#{options[:span].to_s}.png"].join('/')
+    uri
   end
 end
