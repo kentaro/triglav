ea9b0e40fc6fc3c98c0ff179ba4c63b8b35c5fbe
Kentaro Kuribayashi <kentarok@gmail.com>
2012-10-31 16:32:03 +0900

    Added pager to activities_path. #19

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,5 @@
 class ActivitiesController < ApplicationController
   def index
-    @activities = Activity.order('created_at desc')
+    @activities = Activity.order('created_at desc').page(params[:page])
   end
 end
