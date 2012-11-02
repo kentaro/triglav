b7afb6f987c95f4637958657d1ac69daa45ea4ce
Kentaro Kuribayashi <kentarok@gmail.com>
2012-11-02 11:44:22 +0900

    Added `api_token` column to user and use it for authentication.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ActiveRecord::Base
   validates :uid,      presence: true, format: { with: /\A[0-9]+\Z/ }
   validates :image,    presence: true, format: { with: /\A\/\/gravatar\.com\/avatar\/[a-z0-9]{32}\Z/ }
   validates :token,        format: { with: /\A[a-z0-9_\-]{22}\Z/i }, allow_blank: true
-  validates :access_token, format: { with: /\A[a-z0-9]{40}\Z/ }, allow_blank: true
+  validates :access_token, format: { with: /\A[a-z0-9]{40}\Z/ },     allow_blank: true
+  validates :api_token,    format: { with: /\A[a-z0-9_\-]{22}\Z/i }, allow_blank: true
 
   has_many :activities, as: :model
 end
