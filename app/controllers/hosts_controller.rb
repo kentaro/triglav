f9343e3de240defcc0a17c51920c1c065c609a61
Shinya Tsunematsu <s@tnmt.info>
2012-12-11 01:07:21 +0900

    add serial_id to host which define server uniquely.

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -81,7 +81,7 @@ class HostsController < ApplicationController
   private
 
   def host_params
-    params.require(:host).permit(:ip_address, :name, :active, :description, {
+    params.require(:host).permit(:ip_address, :name, :active, :description, :serial_id, {
         host_relations_attributes: [
           :service_id,
           :role_id,
